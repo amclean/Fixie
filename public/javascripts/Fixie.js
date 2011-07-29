8f60363ee7c9758e46be6ba85a7d0564f46b4dad
McLean <199965@alan-mclean-8790-laptop.nyhq.nytint.com>
2011-07-29 18:18:10 -0400

    new files added, ios support

--- public/javascripts/Fixie.js
+++ public/javascripts/Fixie.js
@@ -1,13 +1,24 @@
 (function($){    
   $.fn.fixie = function(options){
-    var $j = jQuery;
-    var me = this;
-    
+    var $j        = jQuery;
+    var me        = this;
+    var isMobile  = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
+
     // bind the events
     this.bindEvents = function(){
-      $j(settings.target).bind(settings.event+'.'+settings.token, settings.callback);
-      $j(window).bind('resize.'+settings.token,this.onReflow);
-      
+
+      if(isMobile){
+        $j(window).bind('orientationchange.'+settings.token,this.onReflow);
+        // $j(settings.target).bind(settings.event+'.'+settings.token, this.evaluateMobile);
+        
+        $j(window).bind('scroll', this.mobileScroll);
+        // $j(window).bind('scroll', this.mobileScroll);
+        
+      }else{
+        $j(window).bind('resize.'+settings.token,this.onReflow);        
+        $j(settings.target).bind(settings.event+'.'+settings.token, this.evaluate);
+      }
+      settings.el.addClass('nytg-fixie');
     };
     
     // triggered on page reflows and resizes
@@ -16,10 +27,27 @@
       me.evaluate();
     };
     
-    // check position of monitored element
+    // event for iOS - start recording the scrolling change
+    this.mobileScroll = function(){
+      var scrollPosition = $j(window).scrollTop();
+      if(scrollPosition >= settings.offset.top && scrollPosition+settings.dimensions.height < settings.container.height){
+        settings.el.css('-webkit-transition-duration','.3s');        
+        settings.el.css('top', (scrollPosition - settings.container.el.offset().top+settings.offsetY) +'px');        
+      }else{
+        if(scrollPosition+settings.dimensions.height >= settings.container.height){
+          settings.el.css({ top: (settings.container.height-settings.dimensions.height)+'px' });
+        }
+      }
+      if(scrollPosition <= settings.offset.top){
+        settings.el.css(settings.original);
+      }
+    };  
+    
+    // check position of monitored element for browsers that DONT support fixed positioning
     this.evaluate = function(){
+      var scrollPosition = $j(window).scrollTop();
       if(settings.fixed === false){
-        if(window.pageYOffset >= settings.offset.top && window.pageYOffset+settings.dimensions.height < settings.container.height){
+        if(scrollPosition >= settings.offset.top && scrollPosition+settings.dimensions.height < settings.container.height){
           settings.el.css({ position: 'fixed', left: settings.offset.left+settings.offsetX, top: settings.offsetY })
           settings.fixed = true;
           settings.glued = false;          
@@ -30,24 +58,25 @@
       }
       
       if(settings.fixed === true){
-        if(window.pageYOffset <= settings.offset.top){     
+        if(scrollPosition <= settings.offset.top){     
           settings.el.css(settings.original);
           settings.fixed = false;
           settings.glued = false;
           settings.onUnFix();
           settings.onUnGlue();
         }
-        if(window.pageYOffset+settings.dimensions.height >= settings.container.height && settings.constrain === true){
-          settings.el.css({ position: 'absolute', right: settings.original.right, left: settings.original.left, top: window.pageYOffset-settings.container.el.offset().top+'px' });          
+        if(scrollPosition+settings.dimensions.height-settings.offset.top >= settings.container.height && settings.constrain === true){
+          settings.el.css({ position: 'absolute', right: settings.original.right, left: settings.original.left, top: settings.offsetY+scrollPosition-settings.container.el.offset().top+'px' });          
           settings.fixed = false;
           settings.glued = true;
           settings.onGlue();
         }
       }
-    };
+    };    
     
     // destroy bound events
     this.destroy = function(){
+      settings.el.removeClass('nytg-fixie');      
       $j(settings.target).unbind(settings.event+'.'+settings.token);
     };    
     
@@ -61,11 +90,13 @@
       callback    : this.evaluate,
       position    : null,
       offset      : null,
+      scrollDelta : 0,
       glued       : false,
       offsetX     : 0,
       offsetY     : 0,
       thresholdX  : 0,
       thresholdY  : 0,
+      backInPlace : true,
       onFix       : function(){ return true; },
       onGlue      : function(){ return true; },
       onUnFix     : function(){ return true; },
@@ -75,8 +106,8 @@
       // random token for namespacing events
       token       : 123
     };
-
-    return this.each(function(){
+    
+    this.each(function(){
       settings.token            = Math.random() * 100000;
       settings.el               = $(this);
       settings.position         = settings.el.position();
@@ -98,10 +129,16 @@
       settings.dimensions       = { 
         width    : settings.el.width(),
         height   : settings.el.height()
-      };      
-      
-      me.bindEvents();
+      };
     });
+    
+    if(settings.el.hasClass('nytg-fixie')){
+      console.log('this')
+      return me;
+    }else{
+      me.bindEvents();
+      return this;
+    }    
 
   };
 })(jQuery);
